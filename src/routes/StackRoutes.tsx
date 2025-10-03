561e631f6bd4399233c08a9e294762d6dc766969
Guilherme DEV <guilhermervisgueira@gmail.com>
2025-10-03 13:44:44 -0300

    adicionando dependencias do paper

--- src/routes/StackRoutes.tsx
+++ src/routes/StackRoutes.tsx
@@ -16,8 +16,6 @@ export default function StackRoutes(){
         component={TelaTeste}
       />
 
-      
-
     </Stack.Navigator>
   );
 }
--- src/screen/TelaLogin.tsx
+++ src/screen/TelaLogin.tsx
@@ -15,18 +15,3 @@ export default function TelaLogin() {
 }
 
 
-
-/* const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 22,
-    fontWeight: 'bold'
-  }
-}); */
-
-
